6aa2439f7bfb973eecbc8d353dd990c27e6b6818
Frank Corona Prendes <frank.corona@pm.me>
2022-11-27 14:02:55 -0300

    chore: catch event from search form and validate render based on doggie value

--- src/components/TheDoggies.vue
+++ src/components/TheDoggies.vue
@@ -1,28 +1,42 @@
 <script setup lang="ts">
 import DoggiesSearchForm from "./DoggiesSearchForm.vue";
 import DoggiesDetails from "./DoggiesDetails.vue";
+</script>
 
-defineProps<{
-  msg: string;
-}>();
+<script lang="ts">
+export default {
+  data() {
+    return {
+      doggie: {},
+    };
+  },
+  methods: {
+    onSearchDoggies(doggieToken: string) {
+      console.log("[🚀::debugger]", doggieToken);
+      this.doggie = {
+        name: "Dogo",
+        description:
+          "This breed has its origin in the province of Cordoba, in the central (Mediterranean) region of the Republic of Argentina. Its creator was Dr.Antonio Nores Martinez",
+        owner: "Dr.Antonio Nores Martinez",
+        thumbnail:
+          "https://www.akc.org/wp-content/uploads/2017/11/Dogo-puppy.jpg",
+        traits: [
+          { trait: "Breed", value: "Dogo" },
+          { trait: "Breed", value: "Dogo" },
+          { trait: "Breed", value: "Dogo" },
+        ],
+      };
+    },
+  },
+};
 </script>
 
 <template>
   <section>
-    <DoggiesSearchForm />
+    <DoggiesSearchForm @doggieToken="onSearchDoggies($event)" />
   </section>
 
-  <section>
-    <DoggiesDetails
-      :dog="{
-        name: 'Dogo',
-        description:
-          'This breed has its origin in the province of Cordoba, in the central (Mediterranean) region of the Republic of Argentina. Its creator was Dr.Antonio Nores Martinez',
-        owner: 'Dr.Antonio Nores Martinez',
-        thumbnail:
-          'https://www.akc.org/wp-content/uploads/2017/11/Dogo-puppy.jpg',
-        traits: [],
-      }"
-    />
+  <section v-if="Object.keys(doggie).length">
+    <DoggiesDetails :doggie="doggie" />
   </section>
 </template>
